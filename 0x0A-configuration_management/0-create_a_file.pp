8bfdea896c25c09093d7c4fb79e469d76b8ca488
not-notAlex <alex.smith1815@gmail.com>
2020-12-07 22:04:26 +0000

    switched order of commands

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,8 +3,8 @@
 file { '/tmp/holberton':
   path    => '/tmp/holberton',
   ensure  => file,
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+  group   => 'www-data'
 }
