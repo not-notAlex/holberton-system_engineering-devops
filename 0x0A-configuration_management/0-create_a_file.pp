6e0797dff96e3e57f21d7902b88cd0969c794e89
not-notAlex <alex.smith1815@gmail.com>
2020-12-10 00:55:43 +0000

    changed order of lines

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates file in the tmp folder
 
 file { '/tmp/holberton':
-  path    => '/tmp/holberton',
   ensure  => file,
+  path    => '/tmp/holberton',
   content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
