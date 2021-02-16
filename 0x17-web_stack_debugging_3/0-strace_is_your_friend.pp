e56d3b2a037cacb095a9d943ecf54a633f69de11
Alex Smith <1763@holbertonschool.com>
2021-02-16 21:24:11 +0000

    initial commit

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fixes a typo in the config file for Apache server
+exec { 'typo fix':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
