fe1cf0e6ac517e79618025190482471b7f959235
Alex Smith <1763@holbertonschool.com>
2021-02-25 19:22:06 +0000

    initial commit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# fixes config file to prevent failed requests
+exec {'max files setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
